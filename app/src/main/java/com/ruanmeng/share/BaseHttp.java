5797d17c6edfdc1258ebfba37148f677ed4e9870
xinnuo <416143467@qq.com>
2018-06-20 18:15:08 +0800

    06-20/18:14 v1.0.6

--- app/src/main/java/com/ruanmeng/share/BaseHttp.java
+++ app/src/main/java/com/ruanmeng/share/BaseHttp.java
@@ -12,7 +12,8 @@ public class BaseHttp {
     private static String baseUrl = BuildConfig.API_HOST;
     private static String baseIp = baseUrl + "/api";
     // public static String baseImg = baseUrl + "/";
-    public static String baseImg = BuildConfig.LOG_DEBUG ? "http://122.114.212.120:8097/" : "http://ebringhome.com/";
+    // public static String baseImg = BuildConfig.LOG_DEBUG ? "http://122.114.212.120:8097/" : "http://ebringhome.com/";
+    public static String baseImg = "http://ebringhome.com/";
 
     public static String login_sub = baseIp + "/login_sub.rm";                       //登录√
     public static String identify_get = baseIp + "/identify_get.rm";                 //注册验证码√
